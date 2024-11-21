a924c472b16d733136d6ba5fcd0dde7437c5fb81
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2024-11-21 12:19:43 +0100

    fix : remove server part

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -5,23 +5,22 @@ import ArticlesList from "../components/ArticlesList";
 import type { ArticleType } from "../lib/definitions";
 
 function HomePage() {
-	const articles: ArticleType[] = useLoaderData() as ArticleType[];
+  const articles: ArticleType[] = useLoaderData() as ArticleType[];
 
-	const [currentCategory, setCurrentCategory] = useState<string>("");
-	console.log(currentCategory);
+  const [currentCategory, setCurrentCategory] = useState<string>("");
 
-	const categories: string[] = [...new Set(articles.map((a) => a.category))];
+  const categories: string[] = [...new Set(articles.map((a) => a.category))];
 
-	return (
-		<>
-			<h1 className="text-4xl text-center my-12">Homepage</h1>
-			<ArticlesFilters
-				categories={categories}
-				setCurrentCategory={setCurrentCategory}
-			/>
-			<ArticlesList articles={articles} currentCategory={currentCategory} />
-		</>
-	);
+  return (
+    <>
+      <h1 className="text-4xl text-center my-12">Homepage</h1>
+      <ArticlesFilters
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+      />
+      <ArticlesList articles={articles} currentCategory={currentCategory} />
+    </>
+  );
 }
 
 export default HomePage;
